3b2177c747e0c5dbfa47ee97b0b7d78801bbae7d
Alok Jha <117200589+axjh03@users.noreply.github.com>
2023-07-19 15:46:57 -0500

    Forget Graphics, let me make Actual backbone first

---
+++ sendemail.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import base64
+from email.message import EmailMessage
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
--- test.py
+++ test.py
@@ -1,22 +1,2 @@
-import pyfiglet
-import time
-
-# Define the frames of your ASCII GIF
-frames = [
-    pyfiglet.figlet_format("Frame 1"),
-    pyfiglet.figlet_format("Frame 2"),
-    pyfiglet.figlet_format("Frame 3"),
-    pyfiglet.figlet_format("Frame 4")
-]
-
-# Set the delay between frames (in seconds)
-delay = 0.5
-
-# Print each frame in a loop
-for frame in frames:
-    # Clear the terminal screen
-    print("\033c", end="")
-    # Print the frame
-    print(frame)
-    # Wait for the specified delay
-    time.sleep(delay)
+import base64 
+import os
